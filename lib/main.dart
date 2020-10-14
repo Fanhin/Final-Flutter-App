f85b94ce4a5e166bf31f055f3fcd8276f17cb5ba
6010110684 <6010110684@R301-mini14.local>
2020-10-14 17:49:13 +0700

    todolist

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 import 'package:trip_buddy/pages/home_page.dart';
+import 'package:trip_buddy/provider/task.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Trip Buddy App',
-      home: HomePage(),
+    return ChangeNotifierProvider(
+      create: (_) => TaskProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primaryColor: Color(0xFFC41A3B),
+            primaryColorLight: Color(0xFFFBE0E6),
+            accentColor: Color(0xFF1B1F32)),
+        home: HomePage(),
+      ),
     );
   }
 }
+
 
