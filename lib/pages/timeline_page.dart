cfd7bbc87f4b39321ae2e6937bd4dd2bb799089d
6010110684 <6010110684@R301-mini14.local>
2020-10-21 17:14:50 +0700

    start class 17.14

--- lib/pages/timeline_page.dart
+++ lib/pages/timeline_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timeline_tile/timeline_tile.dart';
@@ -18,26 +17,26 @@ class _TimelinePageState extends State<TimelinePage> {
       appBar: AppBar(
         title: Text("Timeline Page"),
       ),
-      body: Column(
-        children: <Widget>[
-          TimelineTile(
-            alignment: TimelineAlign.manual,
-            lineXY: 0.3,
-            endChild: Container(
-              constraints: const BoxConstraints(minHeight: 120),
-              color: Colors.lightGreenAccent,
-            ),
-            startChild: Container(
-              child: BlocBuilder<TimelineCubit, List>(
-                builder: (context, state) {
-                  return Text("$state");
-                },
+      body: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (context, index) {
+            return TimelineTile(
+              alignment: TimelineAlign.manual,
+              lineXY: 0.3,
+              endChild: Container(
+                constraints: const BoxConstraints(minHeight: 120),
+                color: Colors.lightGreenAccent,
               ),
-              color: Colors.amberAccent,
-            ),
-          ),
-        ],
-      ),
+              startChild: Container(
+                child: BlocBuilder<TimelineCubit, List>(
+                  builder: (context, state) {
+                    return Text("$state");
+                  },
+                ),
+                color: Colors.amberAccent,
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.remove),
         onPressed: () => {
